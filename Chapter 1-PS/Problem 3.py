2b29731193557b2334b587c55d482d4a06179bca
Muhammad Awais <ranaawaisashraf422@gmail.com>
2026-09-01 22:30:02 +0500

    Delete Chapter 1-PS directory

--- Chapter 1-PS/Problem 3.py
+++
@@ -1,6 +0,0 @@
-import pyttsx3
-
-engine = pyttsx3.init()
-
-engine.say("Hello! Welcome to Awais World.")
-engine.runAndWait()
--- Chapter 1-PS/Problem 4.py
+++
@@ -1,8 +0,0 @@
-import os
-
-path = "/"
-
-contents = os.listdir(path)
-
-for item in contents:
-    print(item)
